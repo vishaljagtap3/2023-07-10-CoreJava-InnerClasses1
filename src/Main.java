86b921f82e9bef6d93a25ef4cd75bb8ade9db5da
aavidsoft-developer <aavidsoft.developer@gmail.com>
2023-08-05 14:25:20 +0530

    Non-Static inner classes 2

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,16 @@ public class Main {
         //new Outer.Inner();
 
         Outer o1 = new Outer(100);
+        o1.display();
         Outer.Inner in11 = o1.new Inner(10);
+        in11.display();
+
+        System.out.println();
 
         Outer o2 = new Outer(200);
+        o2.display();
         Outer.Inner in21 = o2.new Inner(20);
+        in21.display();
 
     }
 }
--- src/Outer.java
+++ src/Outer.java
@@ -33,7 +33,8 @@ public class Outer {
         }
 
         public void display() {
-            System.out.println("in = " + in);
+            System.out.println("in = " + in + " out = " + out);
+            Outer.this.display();
         }
     }
 }
@@ -41,3 +42,8 @@ public class Outer {
 
 //Inner is a class
 //Inner is non-static member of Outer
+//Inner class will have existence as part of outer class object
+//Inner class object will be holding a reference to outer class object, that is why inner class can access the members of outer class object
+//However outer class object will not be holding reference to inner class object
+
+//inner class functions will receive two this, one which refers to inner class object, and other one which refers to outer class object
